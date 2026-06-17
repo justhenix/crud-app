7ef0a042e6f8d33a0a3d6a2dfa33e5c83bf18517
Administrator <gitlab_admin_25f6f1@example.com>
2026-06-17 13:42:45 +0000

    Force HTTPS scheme in AppServiceProvider for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Providers;
+namespace AppProviders;
 
-use Illuminate\Support\ServiceProvider;
+use IlluminateSupportServiceProvider;
+use IlluminateSupportFacadesURL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
